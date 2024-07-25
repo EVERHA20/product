6b61a84f448d076d208ee8948166faf479f7d874
EVERHA20 <87545338+EVERHA20@users.noreply.github.com>
2024-07-24 23:52:03 -0600

    Agregar botones para regresar al index y para borrar

--- src/main/java/com/manuable/app/product/controllers/ProductController.java
+++ src/main/java/com/manuable/app/product/controllers/ProductController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -73,7 +74,7 @@ public class ProductController {
         return "redirect:/products";
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
         try {
             productService.deleteProduct(id);
